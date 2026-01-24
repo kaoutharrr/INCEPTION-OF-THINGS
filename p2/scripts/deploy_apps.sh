bfdff95c75898ba5b1ba8afc2caee56721c0e76a
nouhaila errakeb <nerrakeb@intelvalia.com>
2026-01-25 00:37:09 +0100

    change

--- p2/scripts/deploy_apps.sh
+++ p2/scripts/deploy_apps.sh
@@ -11,9 +11,9 @@ kubectl apply -f /vagrant/confs/app3-deployment.yaml
 kubectl apply -f /vagrant/confs/ingress.yaml
 
 echo "Waiting..."
-kubectl wait --for=condition=available --timeout=120s deployment/app1
-kubectl wait --for=condition=available --timeout=120s deployment/app2
-kubectl wait --for=condition=available --timeout=120s deployment/app3
+kubectl wait --for=condition=available --timeout=120s deployment/app-one
+kubectl wait --for=condition=available --timeout=120s deployment/app-two
+kubectl wait --for=condition=available --timeout=120s deployment/app-three
 
 echo "Applications deployed successfully."
 
