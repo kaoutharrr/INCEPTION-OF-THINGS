fa475b3084e8a42a16a918b099d7a2f3d899f0f5
kaoutharrr <kkouaz04@gmail.com>
2026-01-25 03:25:09 +0100

    changePt3 & Pt2

--- p2/scripts/deploy_apps.sh
+++ p2/scripts/deploy_apps.sh
@@ -2,7 +2,16 @@
 
 echo "Starting deployment of applications..."
 
+echo "Checking files..."
+ls -la /vagrant/confs/
+
+
 sleep 10
+
+if [ ! -f /vagrant/confs/app1-deployment.yaml ]; then
+    echo "ERROR: Files not found in /vagrant/confs/"
+    exit 1
+fi
 
 kubectl apply -f /vagrant/confs/app1-deployment.yaml
 kubectl apply -f /vagrant/confs/app2-deployment.yaml
@@ -11,9 +20,9 @@ kubectl apply -f /vagrant/confs/app3-deployment.yaml
 kubectl apply -f /vagrant/confs/ingress.yaml
 
 echo "Waiting..."
-kubectl wait --for=condition=available --timeout=120s deployment/app1
-kubectl wait --for=condition=available --timeout=120s deployment/app2
-kubectl wait --for=condition=available --timeout=120s deployment/app3
+kubectl wait --for=condition=available --timeout=120s deployment/app-one
+kubectl wait --for=condition=available --timeout=120s deployment/app-two
+kubectl wait --for=condition=available --timeout=120s deployment/app-three
 
 echo "Applications deployed successfully."
 
--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -3,7 +3,7 @@
 echo "Installing k3s..."
 
 #install K3s in server mode
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 
 echo "Wainting..."
 sleep 30
